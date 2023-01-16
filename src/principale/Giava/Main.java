8896de264f246565410528a3ea8124ed26a9e89e
Marco Abbondanza <107813503+Marco9811@users.noreply.github.com>
2023-01-16 11:02:59 +0100

    Update and rename src/main/java/Main.java to src/principale/Giava/Main.java

---
+++ src/principale/Giava/Main.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/?user=root", "root", "Velletri98");
+
+        Statement statement = connection.createStatement();
+
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT last_name, first_name\n" +
+                "FROM newdb.students;\n");
+
+        List <String> surnames = new ArrayList<>();
+
+        while (rs.next()) {
+            System.out.println("Name: " + rs.getString("first_name"));
+            surnames.add(rs.getString("last_name"));
+        }
+
+        for (String surname : surnames) {
+            System.out.println(surname);
+        }
+    }
+}
